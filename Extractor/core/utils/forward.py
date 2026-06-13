08984ee0c8e7c8143cae24cc31211c4e101dee37
creator154 <cteator898@gmail.com>
2026-06-13 18:07:39 +0530

    Update Telegram channel ID in forward.py

--- Extractor/core/utils/forward.py
+++ Extractor/core/utils/forward.py
@@ -2,7 +2,7 @@ from Extractor import app
 
 async def forward_to_log(message, tag="LOG"):
     try:
-        chat_id = -1001234567890  # 👈 REAL Telegram channel ID yaha daal
+        chat_id = -1003728827978  # 👈 REAL Telegram channel ID yaha daal
 
         await app.send_message(
             chat_id=chat_id,
