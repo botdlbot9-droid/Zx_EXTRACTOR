7987d41e90ee1ab29e3c06e07dbd1c7170e4bec3
botdlbot9-droid <botdlbot9@gmail.com>
2026-06-21 18:42:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,51 +1,21 @@
 import os
-from os import getenv
 
+# ----------- REQUIRED VARIABLES (Inko Render/Heroku par set karna hi padega) -----------
+API_ID = int(os.environ.get("API_ID", 0))          # Your API ID from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "")          # Your API Hash
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")        # Your Bot Token from @BotFather
+# -------------------------------------------------------------------------------------
 
-# ------------------------------------------------
-API_ID = int(os.environ.get("API_ID",))
-# ------------------------------------------------
-API_HASH = os.environ.get("API_HASH",)
-# ------------------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN",)
-# ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
+# ----------- OPTIONAL VARIABLES (Defaults diye hain, chaahe toh change karo) ----------
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
 BOT_TEXT = "Sumit_Zx"
-# ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID",))
-# ------------------------------------------------
-# //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("-1003086072844"))
-
-# //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("-1003086072844")) 
-# ------------------------------------------------
-MONGO_URL = os.environ.get("mongodb+srv://itsgoluAPI:jrMHSipToKUEnmcp@cpprivateapi.ghhp3oz.mongodb.net/?appName=CpprivateApi")
-# -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS"))
-# -----------------------------------------------
-join = '<a href="">✳️ JOIN BACKUP</a>'
-# -----------------------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", 6884772962))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1003086072844))
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", -1003086072844))
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://itsgoluAPI:jrMHSipToKUEnmcp@cpprivateapi.ghhp3oz.mongodb.net/?appName=CpprivateApi")
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", 0))
+join = '✳️ JOIN BACKUP'
 UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
-# -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
-# -----------------------------------------------
-ADMIN_BOT_USERNAME = "" #without @
-
+ADMIN_BOT_USERNAME = ""  # without @
 THUMB_URL = os.environ.get("THUMB_URL", "https://files.catbox.moe/vg3vae.jpg")
-
-
-
-
-# # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "22746239"))
-# API_HASH = os.environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "7547829346:AAGyfvOu47EciNhC7NUGSDEDFuBaetYYusw")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "MassRPBot")
-# OWNER_ID = int(os.environ.get("OWNER_ID", "7463601722"))
-# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7463601722").split()))
-# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002601604234"))
-# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002601604234"))
-# THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
-
